ebee6b0a4bdf23a383aac7e3abb517e1094f5658
Daan Verbeek <daanverbeek13@gmail.com>
2024-03-01 11:45:27 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+const int PIN_LED_RED = 4;    // The Number of the red LED pin.
+const int PIN_LED_GREEN = 5;  // The Number of the green LED pin.
+const int PIN_LED_BLUE = 6;   // The Number of the blue LED pin.
+const int PIN_LED_YELLOW = 7; // The Number of the yellow LED pin.
+const int PIN_BUTTON_1 = 8;   // The number of the button 1 (KEY1) pin.
+const int PIN_BUTTON_2 = 9;   // The number of the button 2 (KEY2) pin.
+
+
+void setup() {
+}
+
+void loop() {
+}
